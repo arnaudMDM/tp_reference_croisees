ef7d6ee7d5ca0f57d92085e07315c56bae68bd8b
amerydemon <amerydemon@780ce8b0-9a6f-4037-9a1f-1243e3188808>
2011-12-08 17:37:58 +0000

    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.tpreferencescroisees@39 780ce8b0-9a6f-4037-9a1f-1243e3188808

--- TpReferencesCroisees/tests.sh
+++ TpReferencesCroisees/tests.sh
@@ -1,41 +1,54 @@
-echo "Test 1.1\n"
-./TP_STL.exe -k keywords.txt fileTest1.txt -e > diff 
+echo "Test 1.1"
+./TP_STL.exe -k keywords.txt fileTest1.txt -e
 
-echo "Test 1.2\n"
+echo "Test 1.2"
 ./TP_STL.exe -k keywords.txt -e fileTest1.txt
 
-echo "Test 1.3\n"
+echo "Test 1.3"
 ./TP_STL.exe -k -e keywords.txt fileTest1.txt
 
-echo "Test 1.4\n"
+echo "Test 1.4"
 ./TP_STL.exe -e keywords.txt -k fileTest1.txt
 
-echo "Test 1.5\n"
+echo "Test 1.5"
 ./TP_STL.exe
 
-echo "Test 1.6\n"
+
+echo "Test 1.6"
 ./TP_STL.exe -e -k keywords.txt
 
-echo "Test 1.7\n"
+echo "Test 1.7"
 ./TP_STL.exe -f -k keywords.txt
 
-echo "Test 2\n"
+echo "Test 2"
 ./TP_STL.exe fileTest2.txt fileTest2.txt
 
-echo "Test 3\n"
+echo "Test 3"
 ./TP_STL.exe fileTest3.txt
 
-echo "Test 4\n"
+echo "Test 4"
 ./TP_STL.exe -k keywords2.txt fileTest1.txt
 
-echo "Test 5\n"
+echo "Test 5"
 ./TP_STL.exe fileTest4.txt
 
-echo "Test 6\n"
+echo "Test 6"
 ./TP_STL.exe -e -k keywords3.txt fileTest1.txt
 
-echo "Test 7\n"
+echo "Test 7"
 ./TP_STL.exe -e -k key.txt fileTest1.txt
 
-echo "Test 8\n"
+echo "Test 8"
 ./TP_STL.exe -e fileTest1.txt fileTest2.txt fileTest3.txt toto.txt
+
+echo "Test 9"
+./TP_STL.exe -e -k keywords.txt fileTest1.txt fileTest2.txt
+
+echo "Test 10"
+./TP_STL.exe -e fileTest1.txt fileTest2.txt
+
+echo "Test 11"
+./TP_STL.exe -k keywords.txt fileTest1.txt fileTest2.txt
+
+echo "Test 12"
+./TP_STL.exe fileTest1.txt fileTest2.txt
