662a5cdac22db477bf91c5fac35c44427351553c
rgicquel <rgicquel@780ce8b0-9a6f-4037-9a1f-1243e3188808>
2011-12-08 16:20:10 +0000

    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.tpreferencescroisees@35 780ce8b0-9a6f-4037-9a1f-1243e3188808

--- TpReferencesCroisees/tests.sh
+++ TpReferencesCroisees/tests.sh
@@ -1,41 +1,41 @@
 echo "Test 1.1\n"
-TP_STL.exe -k keywords.txt fileTest1.txt -e
+./TP_STL.exe -k keywords.txt fileTest1.txt -e > diff 
 
 echo "Test 1.2\n"
-TP_STL.exe -k keywords.txt -e fileTest1.txt
+./TP_STL.exe -k keywords.txt -e fileTest1.txt
 
 echo "Test 1.3\n"
-TP_STL.exe -k -e keywords.txt fileTest1.txt
+./TP_STL.exe -k -e keywords.txt fileTest1.txt
 
 echo "Test 1.4\n"
-TP_STL.exe -e keywords.txt -k fileTest1.txt
+./TP_STL.exe -e keywords.txt -k fileTest1.txt
 
 echo "Test 1.5\n"
-TP_STL.exe
+./TP_STL.exe
 
 echo "Test 1.6\n"
-TP_STL.exe -e -k keywords.txt
+./TP_STL.exe -e -k keywords.txt
 
 echo "Test 1.7\n"
-TP_STL.exe -f -k keywords.txt
+./TP_STL.exe -f -k keywords.txt
 
 echo "Test 2\n"
-TP_STL.exe fileTest2.txt fileTest2.txt
+./TP_STL.exe fileTest2.txt fileTest2.txt
 
 echo "Test 3\n"
-TP_STL.exe fileTest3.txt
+./TP_STL.exe fileTest3.txt
 
 echo "Test 4\n"
-TP_STL.exe -k keywords2.txt fileTest1.txt
+./TP_STL.exe -k keywords2.txt fileTest1.txt
 
 echo "Test 5\n"
-TP_STL.exe fileTest4.txt
+./TP_STL.exe fileTest4.txt
 
 echo "Test 6\n"
-TP_STL.exe -e -k keywords3.txt fileTest1.txt
+./TP_STL.exe -e -k keywords3.txt fileTest1.txt
 
 echo "Test 7\n"
-TP_STL.exe -e -k key.txt fileTest1.txt
+./TP_STL.exe -e -k key.txt fileTest1.txt
 
 echo "Test 8\n"
-TP_STL.exe -e fileTest1.txt fileTest2.txt fileTest3.txt toto.txt
+./TP_STL.exe -e fileTest1.txt fileTest2.txt fileTest3.txt toto.txt
